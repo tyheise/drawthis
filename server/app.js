1db54d23b49648b66f1ea84a3538b24327de7a3e
Tyler <tyler.heise@gmail.com>
2021-09-02 02:36:56 +0000

    First commit with rough project structure. Node server will serve React app unless accessed via an api URL.

---
+++ server/app.js
@@ -0,0 +1,27 @@
+// server/index.js
+
+const express = require("express");
+const path = require("path");
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+// Have Node serve the files for our built React app
+parent_dir = path.resolve(__dirname, "..")
+app.use(express.static(path.resolve(parent_dir, 'client/build/')));
+
+// Handle GET requests to /api route
+app.get("/api", (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.json({ message: "Hello from server!" });
+});
+
+// All other GET requests not handled before will return our React app
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});
